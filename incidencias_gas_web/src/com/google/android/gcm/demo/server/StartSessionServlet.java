c8c908a69d0ac87c4ae4d02e0305b740af1f05b2
AntonioDiaz <antonio.diaz.arroyo@gmail.com>
2013-04-29 17:21:51 +0200

    testing login

--- incidencias_gas_web/src/com/google/android/gcm/demo/server/StartSessionServlet.java
+++ incidencias_gas_web/src/com/google/android/gcm/demo/server/StartSessionServlet.java
@@ -7,14 +7,31 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+
 public class StartSessionServlet extends BaseServlet {
 
 	private static final long serialVersionUID = 1L;
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		UserService userService = UserServiceFactory.getUserService();
+		  String thisURL = req.getRequestURI();
+		if (req.getUserPrincipal() != null) {
+            resp.getWriter().println("<p>Hello, " +
+                                     req.getUserPrincipal().getName() +
+                                     "!  You can <a href=\"" +
+                                     userService.createLogoutURL(thisURL) +
+                                     "\">sign out</a>.</p>");
+        } else {
+            resp.getWriter().println("<p>Please <a href=\"" +
+                                     userService.createLoginURL(thisURL) +
+                                     "\">sign in</a>.</p>");
+        }
+		
 		/* Should check user and password. */
-		RequestDispatcher rd = getServletContext().getRequestDispatcher("/home.jsp");
-		rd.forward(req, resp);
+		//RequestDispatcher rd = getServletContext().getRequestDispatcher("/home.jsp");
+		//rd.forward(req, resp);
 	}
 }
