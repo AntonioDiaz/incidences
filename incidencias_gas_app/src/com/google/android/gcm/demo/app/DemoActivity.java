b942e26fa31b78893cf487201dc3a683a8384d88
AntonioDiaz <antonio.diaz.arroyo@gmail.com>
2013-05-05 09:34:23 +0200

    Cleaning code.

--- incidencias_gas_app/src/com/google/android/gcm/demo/app/DemoActivity.java
+++ incidencias_gas_app/src/com/google/android/gcm/demo/app/DemoActivity.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2012 Google Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.google.android.gcm.demo.app;
 
 import static com.google.android.gcm.demo.app.CommonUtilities.DISPLAY_MESSAGE_ACTION;
@@ -29,6 +14,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -47,39 +33,37 @@ public class DemoActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		checkNotNull(SERVER_URL, "SERVER_URL");
 		checkNotNull(SENDER_ID, "SENDER_ID");
-		// Make sure the device has the proper dependencies.
+		/* Make sure the device has the proper dependencies. */
 		GCMRegistrar.checkDevice(this);
-		// Make sure the manifest was properly set - comment out this line
-		// while developing the app, then uncomment it when it's ready.
+		/* Make sure the manifest was properly set - comment out this line while developing the app, then uncomment it when it's ready. */
 		GCMRegistrar.checkManifest(this);
 		setContentView(R.layout.main);
 		mDisplay = (TextView) findViewById(R.id.display);
 		registerReceiver(mHandleMessageReceiver, new IntentFilter(DISPLAY_MESSAGE_ACTION));
 		final String regId = GCMRegistrar.getRegistrationId(this);
 		if (regId.equals("")) {
-			// Automatically registers application on startup.
+			/* Automatically registers application on startup. */
+
 			GCMRegistrar.register(this, SENDER_ID);
 		} else {
-			// Device is already registered on GCM, check server.
+			/* Device is already registered on GCM, check server. */
 			if (GCMRegistrar.isRegisteredOnServer(this)) {
-				// Skips registration.
+				/* Skips registration. */
 				mDisplay.append(getString(R.string.already_registered) + "\n");
 			} else {
-				// Try to register again, but not in the UI thread.
-				// It's also necessary to cancel the thread onDestroy(),
-				// hence the use of AsyncTask instead of a raw thread.
+				/* Try to register again, but not in the UI thread.
+				 * It's also necessary to cancel the thread onDestroy(), hence the use of AsyncTask instead of a raw thread. */
 				final Context context = this;
 				mRegisterTask = new AsyncTask<Void, Void, Void>() {
 
 					@Override
 					protected Void doInBackground(Void... params) {
-						boolean registered = ServerUtilities.register(context, regId);
-						// At this point all attempts to register with the app
-						// server failed, so we need to unregister the device
-						// from GCM - the app will try to register again when
-						// it is restarted. Note that GCM will send an
-						// unregistered callback upon completion, but
-						// GCMIntentService.onUnregistered() will ignore it.
+						TelephonyManager telephonyManager = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
+						String number = telephonyManager.getLine1Number();						
+						boolean registered = ServerUtilities.register(context, regId, number);
+						/* At this point all attempts to register with the app server failed, so we need to unregister the device 
+						 * from GCM - the app will try to register again when it is restarted. Note that GCM will send an
+						 * unregistered callback upon completion, but GCMIntentService.onUnregistered() will ignore it. */
 						if (!registered) {
 							GCMRegistrar.unregister(context);
 						}
@@ -90,7 +74,6 @@ public class DemoActivity extends Activity {
 					protected void onPostExecute(Void result) {
 						mRegisterTask = null;
 					}
-
 				};
 				mRegisterTask.execute(null, null, null);
 			}
@@ -107,17 +90,15 @@ public class DemoActivity extends Activity {
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
-		/*
-		 * Typically, an application registers automatically, so options below
-		 * are disabled. Uncomment them if you want to manually register or
-		 * unregister the device (you will also need to uncomment the equivalent
-		 * options on options_menu.xml).
-		 */
-		/*
-		 * case R.id.options_register: GCMRegistrar.register(this, SENDER_ID);
-		 * return true; case R.id.options_unregister:
-		 * GCMRegistrar.unregister(this); return true;
-		 */
+		/* Typically, an application registers automatically, so options below are disabled. 
+		 * Uncomment them if you want to manually register or unregister the device 
+		 * (you will also need to uncomment the equivalent options on options_menu.xml). */
+		case R.id.options_register: 
+			GCMRegistrar.register(this, SENDER_ID);
+			return true; 
+		case R.id.options_unregister:
+			GCMRegistrar.unregister(this); 
+			return true;
 		case R.id.options_clear:
 			mDisplay.setText(null);
 			return true;
