dc3c2f088976bcfdec456adb0ba6c0994202d92e
AntonioDiaz <antonio.diaz.arroyo@gmail.com>
2013-05-25 17:15:52 +0200

    Fixing problem on updating location

--- incidencias_gas_web/src/com/incidences/servlets/UpdateLocationServlet.java
+++ incidencias_gas_web/src/com/incidences/servlets/UpdateLocationServlet.java
@@ -1,13 +1,11 @@
 package com.incidences.servlets;
 
 import java.io.IOException;
-import java.util.logging.Logger;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.google.appengine.api.datastore.KeyFactory;
 import com.incidences.entities.Technician;
 import com.incidences.entities.TechnicianDao;
 import com.incidences.entities.TechnicianDaoImpJdo;
@@ -20,21 +18,19 @@ import com.incidences.entities.TechnicianDaoImpJdo;
 public class UpdateLocationServlet extends BaseServlet {
 
 	private static final long serialVersionUID = 1L;
-	private static final Logger logger = Logger.getLogger(UpdateLocationServlet.class.getName());
+	//private static final Logger logger = Logger.getLogger(UpdateLocationServlet.class.getName());
 
 	/* update technician location. */
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String idTechnician = getParameter(req, "id_technician");
 		TechnicianDao technicianDao = new TechnicianDaoImpJdo();
-		Technician technician = technicianDao.getTechnician(KeyFactory.stringToKey(idTechnician));
-		
+		Technician technician = technicianDao.getTechnician(idTechnician);		
 		String newLatitude = getParameter(req, "new_latitude");
 		String newLongitude = getParameter(req, "new_longitude");
 		technician.setLongitude(newLongitude);
 		technician.setLatitude(newLatitude);
-		technicianDao.updateTechnician(KeyFactory.stringToKey(idTechnician), technician);
-		
+		technicianDao.updateTechnician(technician.getKey(), technician);		
 		resp.setStatus(HttpServletResponse.SC_OK);
 	}
 }
